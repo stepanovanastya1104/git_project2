3ff19ed945375bef6aec8ba903291869aed62cfd
yandex-2021 <Nastya.stepanova1104@yandex.ru>
2022-12-01 15:28:34 +0300

    Second commit

--- my.py
+++ my.py
@@ -3,5 +3,9 @@ def main_function():
     print('And I change it every day')
 
 
+def second_function():
+    print('16 декабря - самый лучший день!')
+
+
 if __name__ == '__main__':
     main_function()
